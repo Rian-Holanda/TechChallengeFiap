0dec68f46c0d059b5d59ba8441f5e3c99b1ef466
Rian-Holanda <rian.holanda@rvc.law>
2025-02-04 03:03:40 -0300

    Configuração de controller Consulta

--- API_TechChallengeFiap/Controllers/ConsultaController.cs
+++ API_TechChallengeFiap/Controllers/ConsultaController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess_TechChallengeFiap.Consultas.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_TechChallengeFiap.Controllers
 {
     public class ConsultaController : Controller
     {
+        private readonly IConsultaCommand _consultaCommand;
+        private readonly IConsultaQueries _consultaQueries;
+        
+        public ConsultaController(IConsultaCommand consultaCommand, IConsultaQueries consultaQueries)
+        {
+           _consultaCommand = consultaCommand;
+           _consultaQueries = consultaQueries;
+        }
+
         public IActionResult Index()
         {
             return View();
--- API_TechChallengeFiap/Program.cs
+++ API_TechChallengeFiap/Program.cs
@@ -1,5 +1,10 @@
+using DataAccess_TechChallengeFiap.Consultas.Commands;
 using DataAccess_TechChallengeFiap.Consultas.Interface;
 using DataAccess_TechChallengeFiap.Consultas.Queries;
+using DataAccess_TechChallengeFiap.Medico.Command;
+using DataAccess_TechChallengeFiap.Medico.Interfaces;
+using DataAccess_TechChallengeFiap.Paciente.Command;
+using DataAccess_TechChallengeFiap.Paciente.Interfaces;
 using Infrastructure_FiapTechChallenge;
 using Infrastructure_FiapTechChallenge.Config;
 using Microsoft.AspNetCore.Identity;
@@ -30,6 +35,9 @@ builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options =>
 
 builder.Services.AddScoped<IConsultaQueries, ConsultaQueries>();
 builder.Services.AddScoped<IConsultaCommand, ConsultaCommand>();
+builder.Services.AddScoped<IPacienteCommand, PacienteCommand>();
+builder.Services.AddScoped<IMedicoCommand,   MedicoCommand>();
+
 
 
 builder.Services.AddControllers();
